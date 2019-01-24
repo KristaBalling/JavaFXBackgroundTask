bbde4c1c4ed77fced5a7010baa779a4d15af39a3
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-24 16:05:58 -0500

    replaced task with service

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -10,8 +11,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 
 public class Controller {
-
-    private Task<ObservableList<String>> task;
 
     @FXML
     private ListView listView;
@@ -22,39 +21,43 @@ public class Controller {
     @FXML
     private Label progressLabel;
 
+    @FXML
+    private Service<ObservableList<String>> service;
+
     public void initialize() {
-        task = new Task<ObservableList<String>>() {
-            @Override
-            protected ObservableList<String> call() throws Exception {
+//        task = new Task<ObservableList<String>>() {
+//            @Override
+//            protected ObservableList<String> call() throws Exception {
 
-                String[] names = {"Tim Buchalka",
-                        "Bill Rogers",
-                        "Jack Jill",
-                        "Joan Andrews",
-                        "Mary Johnson",
-                        "Bob McDonald"};
+//                String[] names = {"Tim Buchalka",
+//                        "Bill Rogers",
+//                        "Jack Jill",
+//                        "Joan Andrews",
+//                        "Mary Johnson",
+//                        "Bob McDonald"};
+//
+//                 ObservableList<String> employees = FXCollections.observableArrayList();
+//
+//                 for(int i=0; i<6; i++) {
+//                     employees.add(names[i]);
+//                     updateMessage("Added " + names[i] + " to the list");
+//                     updateProgress(i + 1, 6);
+//                     Thread.sleep(200);
+//                 }
+//
+//                return employees;
+//            }
+//        };
 
-                 ObservableList<String> employees = FXCollections.observableArrayList();
-
-                 for(int i=0; i<6; i++) {
-                     employees.add(names[i]);
-                     updateMessage("Added " + names[i] + " to the list");
-                     updateProgress(i + 1, 6);
-                     Thread.sleep(200);
-                 }
-
-                return employees;
-            }
-        };
-
-        progressBar.progressProperty().bind(task.progressProperty());
-        progressLabel.textProperty().bind(task.messageProperty());
-        listView.itemsProperty().bind(task.valueProperty());
+        service = new EmployeeService();
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
+        listView.itemsProperty().bind(service.valueProperty());
     }
 
         @FXML
         public void buttonPressed() {
-            new Thread(task).start();
+            service.start();
 
 
     }
